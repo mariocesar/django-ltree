fac30f346bae7ff940f4aff4386489fb6cbad38d
Mario César Señoranis Ayala <mariocesar.c50@gmail.com>
2018-10-06 12:13:11 -0400

    Custom form field

--- django_ltree/fields.py
+++ django_ltree/fields.py
@@ -1,7 +1,27 @@
 from django.db.models.fields import TextField
+from django.core.validators import RegexValidator
+from django import forms
+from django.forms.widgets import TextInput
+
+
+path_label_validator = RegexValidator(
+    r'^[A-Za-z0-9_.]+$',
+    'A label is a sequence of alphanumeric characters and underscores separated by dots.',
+    'invalid')
+
+
+class PathFormField(forms.CharField):
+    default_validators = [path_label_validator]
 
 
 class PathField(TextField):
+    default_validators = [path_label_validator]
+
     def db_type(self, connection):
         return 'ltree'
+
+    def formfield(self, **kwargs):
+        kwargs['form_class'] = PathFormField
+        kwargs['widget'] = TextInput(attrs={'class': 'vTextField'})
+        return super().formfield(**kwargs)
 
--- example/categories/models.py
+++ example/categories/models.py
@@ -6,6 +6,16 @@ class Category(models.Model):
     name = models.CharField(max_length=140)
     path = PathField(unique=True)
 
+    class Meta:
+        verbose_name_plural = 'categories'
+        ordering = ('path', )
+
     def __str__(self):
         return self.path
 
+    def subcategories(self):
+        # TODO: Implement lookups
+        return Category.objects.filter(
+            path__descendant=self.path,
+            path__nlevel=NLevel(self.path) + 1
+        )
