eba4b92520761ede686222a3538d4c167f24beed
Mario César Señoranis Ayala <mariocesar.c50@gmail.com>
2018-10-06 11:06:57 -0400

    fix missing comma

--- django_ltree/checks.py
+++ django_ltree/checks.py
@@ -8,7 +8,7 @@ def check_database_backend_is_postgres(app_configs, **kwargs):
 
     if 'postgres' not in settings.DATABASES['default']['ENGINE']:
         errors.append(Warning(
-            'django_ltree needs postgres to support install the ltree extension.'
+            'django_ltree needs postgres to support install the ltree extension.',
             hint='Use the postgres engine or ignore if you already use a custom engine for postgres'))
 
     return errors
