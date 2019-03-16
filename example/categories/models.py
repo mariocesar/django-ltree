8e86231b8db5d9f0a749529ed40054641b116f4d
Mario César Señoranis Ayala <mariocesar.c50@gmail.com>
2019-03-16 16:23:20 -0400

    Update example

--- example/categories/models.py
+++ example/categories/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+
 from django_ltree.fields import PathField
+from django_ltree.functions import NLevel
 
 
 class Category(models.Model):
@@ -7,15 +9,12 @@ class Category(models.Model):
     path = PathField(unique=True)
 
     class Meta:
-        verbose_name_plural = 'categories'
-        ordering = ('path', )
+        verbose_name_plural = "categories"
 
     def __str__(self):
         return self.path
 
     def subcategories(self):
-        # TODO: Implement lookups
         return Category.objects.filter(
-            path__descendant=self.path,
-            path__nlevel=NLevel(self.path) + 1
+            path__descendant=self.path, path__nlevel=NLevel(self.path) + 1
         )
