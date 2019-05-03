14b3f6b0e5072ef13761b22a0406d109e7c698ce
Mario César <mariocesar.c50@gmail.com>
2019-05-03 09:42:20 -0400

    Setup build and release info

--- setup.py
+++ setup.py
@@ -1,27 +1,35 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
-    name='django_ltree',
-    version='0.3',
-    python_requires='>=3.6',
-    url='https://github.com/mariocesar/django-ltree',
-    author='Mario César Señoranis Ayala',
-    description='A django that implements in a model the ltree postgres extension',
-    license='MIT',
+    name="django_ltree",
+    version="0.3",
+    python_requires=">=3.6",
+    url="https://github.com/mariocesar/django-ltree",
+    author="Mario César Señoranis Ayala",
+    author_email="mariocesar@humanzilla.com",
+    description="A django that implements in a model the ltree postgres extension",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
     packages=find_packages(),
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3 :: Only',
+        "Development Status :: 4 - Beta",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
     ],
     project_urls={
-        'Source': 'https://github.com/mariocesar/django_ltree',
-        'Tracker': 'https://github.com/mariocesar/django_ltree/issues',
+        "Source": "https://github.com/mariocesar/django_ltree",
+        "Tracker": "https://github.com/mariocesar/django_ltree/issues",
     },
 )
