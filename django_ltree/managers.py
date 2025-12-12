a6c05b2fe4ffcc1f58d8793fa0182cee2a151d9b
amirreza <amir.rsf1380@gmail.com>
2025-12-12 13:36:23 +0330

    use id to create tree path

--- django_ltree/managers.py
+++ django_ltree/managers.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from django_ltree.paths import PathGenerator
+from django_ltree.fields import PathValue
 
 
 class TreeQuerySet(models.QuerySet):
@@ -22,12 +22,16 @@ class TreeManager(models.Manager):
         return self.filter().children(path)
 
     def create_child(self, parent=None, **kwargs):
-        paths_in_use = parent.children() if parent else self.roots()
         prefix = parent.path if parent else None
-        path_generator = PathGenerator(
-            prefix,
-            skip=paths_in_use.values_list("path", flat=True),
-            label_size=getattr(self.model, "label_size"),
-        )
-        kwargs["path"] = next(path_generator)
-        return self.create(**kwargs)
+        kwargs.pop("path", None)
+        obj = self.create(**kwargs)
+
+        if prefix:
+            path = PathValue([*prefix, obj.id])
+        else:
+            path = PathValue([obj.id])
+        self.filter(id=obj.id).update(path=path)
+
+        obj.path = path
+
+        return obj
