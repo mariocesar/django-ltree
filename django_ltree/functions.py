d195dba07eff17d1f46e73965b5968fb05927143
amirreza <amir.rsf1380@gmail.com>
2025-12-17 17:15:37 +0330

    add `Subpath` and `Nlevel` functions

--- django_ltree/functions.py
+++ django_ltree/functions.py
@@ -1,15 +1,26 @@
-from django.db.models import Transform, fields
+from django.db.models import Value, fields
+from django.db.models.expressions import Func
 
 from .fields import PathField
 
-__all__ = ("NLevel",)
+
+class NLevel(Func):
+    function = "NLEVEL"
+    output_field = fields.IntegerField()
+    arity = 1
+
+    def __init__(self, expression, **extra):
+        super().__init__(expression, **extra)
 
 
-@PathField.register_lookup
-class NLevel(Transform):
-    lookup_name = "depth"
-    function = "nlevel"
+class Subpath(Func):
+    function = "SUBPATH"
+    output_field = PathField()
 
-    @property
-    def output_field(self):
-        return fields.IntegerField()
+    def __init__(self, *expressions, output_field=None, **extra):
+        if len(expressions) != 2 and len(expressions) != 3:
+            raise ValueError("Subpath takes either 2 or 3 arguments")
+        super().__init__(*expressions, output_field=output_field, **extra)
+
+
+__all__ = ("NLevel", "Subpath")
