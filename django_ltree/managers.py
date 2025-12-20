b80a02487ed959fb9dc82f6e31a9c8bd922e8271
amirreza <amir.rsf1380@gmail.com>
2025-12-20 23:28:44 +0330

    add `_create` which holds common creation logic

--- django_ltree/managers.py
+++ django_ltree/managers.py
@@ -35,3 +35,24 @@ class TreeManager(models.Manager):
         obj.path = path
 
         return obj
+
+    def _create(self, **kwargs):
+        """
+        Create a new object with the given kwargs, saving it to the database
+        and returning the created object.
+        """
+        reverse_one_to_one_fields = frozenset(kwargs).intersection(
+            self.model._meta._reverse_one_to_one_field_names
+        )
+        if reverse_one_to_one_fields:
+            raise ValueError(
+                "The following fields do not exist in this model: %s"
+                % ", ".join(reverse_one_to_one_fields)
+            )
+
+        obj = self.model(**kwargs)
+        self._for_write = True
+        obj.save(force_insert=True, using=self.db)
+        return obj
+
+    _create.alters_data = True
